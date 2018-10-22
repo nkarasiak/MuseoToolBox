1b649cc345e9b9c61eb510fabc8c989cb8cc2c59
Nicolas Karasiak <nicolas.karasiak@inra.fr>
2018-10-22 14:06:42 +0200

    Fix : random selection for randomSampleVector

--- tools/randomSampleVector.py
+++ tools/randomSampleVector.py
@@ -17,6 +17,7 @@ import vectorTools
 import os
 import numpy as np
 
+
 class samplingMethods:    
     def standCV(inStand,SLOO=True,maxIter=False,seed=None):
         """
@@ -50,7 +51,7 @@ class samplingMethods:
         samplingType = 'SLOO'
         return [samplingType,dict(coordinateMatrix=coordinateMatrix,distanceThresold=distanceThresold,minTrain=minTrain,SLOO=SLOO,maxIter=maxIter,seed=seed)]
         
-    def random(split=0.5,maxSamples=None,seed=None,nIter=5):
+    def random(split=0.5,maxSamplesPerClass=None,seed=None,nIter=5):
         """
         split : float,int. Default 0.5.
             If float from 0.1 to 0.9 (means keep 90% for training). If int, will try to reach this sample for every class.
@@ -63,7 +64,7 @@ class samplingMethods:
             Number of iteration of the random sampling (will add 1 to the seed at each iteration if defined).
         """
         samplingType = 'random'
-        return [samplingType,dict(split=split,maxSamples=maxSamples,seed=seed,nIter=nIter)]
+        return [samplingType,dict(split=split,maxSamplesPerClass=maxSamplesPerClass,seed=seed,nIter=nIter)]
 
     def extractCoordinatesMatrixForSLOOCV(inVector,inRaster):
         #TODO
@@ -102,43 +103,46 @@ class sampleSelection:
         self.__alertMessage = 'It seems you already generated the cross validation. \n Please use reinitialize function if you want to regenerate the cross validation. \n But check if you defined a seed number in your samplingMethods function to have the same output.'
         self.__alreadyRead = False
         
+        # create unique randil state if no seed
+        if self.samplingMethod[1]['seed'] is None:
+            import time
+            self.samplingMethod[1]['seed'] = int(time.time())
+            
         ### Totally random
         if self.samplingType == 'random':
+            global FIDs
             FIDs,self.fts,self.srs= vectorTools.readROIFromVector(inVector,inField,getFeatures=True,srs=True)
             FIDs = FIDs.flatten()
             from sklearn.model_selection import train_test_split
             nIter = samplingMethod[1]['nIter']
             seed = samplingMethod[1]['seed']
             train_size = samplingMethod[1]['split']
-            maxSamples = samplingMethod[1]['maxSamples']
-            if isinstance(maxSamples,str):
-                nSamples = np.amin(np.unique(FIDs,return_counts=True)[1])
-                def train_test_eq_split(X, y, n_per_class, random_state=None):
-                    if random_state:
-                        np.random.seed(random_state)
-                    sampled = X.groupby(y, sort=False).apply(
-                        lambda frame: frame.sample(n_per_class))
-                    mask = sampled.index.get_level_values(1)
+            maxSamplesPerClass = samplingMethod[1]['maxSamplesPerClass']
+            if maxSamplesPerClass:
+                def randomSamplePerClass(Cpos,nSamp):
+                    train = np.random.permutation(Cpos)[:nSamp]
+                    TF = np.in1d(Cpos,train,invert=True)
+                    valid = Cpos[TF]
+                    return [train,valid]
+                def randomPerClass(FIDs,maxSamples,seed):
+                    train,valid = [np.asarray([]),np.asarray([])]
+                    for C in np.unique(FIDs):
+                        Cpos=np.where(FIDs==C)[0]
+                        np.random.seed(seed)
+                        trvl = randomSamplePerClass(Cpos,maxSamples)
+                        train = np.concatenate((train,trvl[0]))
+                        valid = np.concatenate((valid,trvl[1]))
+                    return train,valid
                 
-                    X_train = X.drop(mask)
-                    X_test = X.loc[mask]
-                    y_train = y.drop(mask)
-                    y_test = y.loc[mask]
-
-                return X_train, X_test, y_train, y_test
-                train_test_eq_split(FIDs,random_state=seed)
-
-            if isinstance(seed,int):
-                self.crossvalidation = [train_test_split(FIDs,train_size=train_size,random_state=samplingMethod[1]['seed']+i) for i in range(nIter)]
+                self.crossvalidation = [randomPerClass(FIDs,maxSamplesPerClass,seed+i) for i in range(nIter)]
             else:
-                self.crossvalidation = [train_test_split(FIDs,train_size=train_size,random_state=i) for i in range(nIter)]
-            
-        ### Split at maximum distance beyond each point
+                self.crossvalidation = [train_test_split(FIDs,train_size=train_size,random_state=seed+i) for i in range(nIter)]            
         
+        ### Split at maximum distance beyond each point
         ### For Spatial-Leave-One-Out
         if self.samplingType == 'SLOO':
             self.crossvalidation = vectorTools.distanceCV(**samplingMethod[1])
-        
+            
         ## For Stand Split    
         if self.samplingType == 'STAND':
             inStand = samplingMethod[1]['inStand']
@@ -157,9 +161,9 @@ class sampleSelection:
         for idx,ext in enumerate(self.extensions):
             print(3*' '+'- '+self.driversName[idx]+' : '+ext)
     
-    def getCrossValidation(self):
+    def getCrossValidationForScikitLearn(self):
         if self.__alreadyRead is True:
-            print(self.__alertMessage)
+            raise Warning(self.__alertMessage)
         else:
             self.__alreadyRead = True
             return self.crossvalidation
@@ -180,11 +184,8 @@ class sampleSelection:
                 self.cv = []
                 for idx,trvl in enumerate(self.crossvalidation):
                     self.cv.append([trvl[0],trvl[1]])
-                    for i in trvl[0]:
-                        print(i)
-                        print(i.shape)
-                    trFeat = [self.fts[i] for i in trvl[0]]
-                    vlFeat= [self.fts[i] for i in trvl[1]]
+                    trFeat = [self.fts[int(i)] for i in trvl[0]]
+                    vlFeat= [self.fts[int(i)] for i in trvl[1]]
                     tr = self.__fileName+'_train_'+str(idx)+self.__ext
                     vl = self.__fileName+'_valid_'+str(idx)+self.__ext
                     self.__saveToShape__(trFeat,self.srs,tr)
@@ -247,13 +248,11 @@ if __name__ == '__main__':
     inStand = 'spjoin_rif'
     
     
-    sampling = samplingMethods.standCV(inStand,SLOO=True,maxIter=3,seed=1)
-    sampling = samplingMethods.random('balanced',nIter=10)
+    #sampling = samplingMethods.standCV(inStand,SLOO=True,maxIter=3,seed=1)
+    sampling = samplingMethods.random(split=0.5,nIter=5)
     rsv = sampleSelection(sampling)
-    files = rsv.saveVectorFiles('/tmp/nana.sqlite')
-    """
-    cv = rsv.getCrossValidation()
-    for tr,vl in cv:
-        print(tr.shape)
-        print(vl.shape)
-    """
+    #cv = rsv.getCrossValidationForScikitLearn()
+    files = rsv.saveVectorFiles('/tmp/test.sqlite')
+    
+    for tr,vl in files:
+        print(tr,vl)
