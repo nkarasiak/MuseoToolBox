c97b1f03eca5440d96a685989781902c5f9bba69
Nicolas Karasiak <nicart@gmail.com>
2019-09-05 12:32:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setuptools.setup(
             'mtb_modalClass=museotoolbox.stats.computeClassificationStability:modalClassCLI'
         ],
     },
-    data_files=[('tiff', ['museotoolbox/datasets/historicalmap/map_compress.tif']),('gpkg', ['museotoolbox/datasets/historicalmap/train.gpkg','museotoolbox/datasets/historicalmap/train_centroid.gpkg'])],
+    #data_files=[('tiff', ['museotoolbox/datasets/historicalmap/map_compress.tif']),('gpkg', ['museotoolbox/datasets/historicalmap/train.gpkg','museotoolbox/datasets/historicalmap/train_centroid.gpkg'])],
     data_files=[('tiff', ['datasets/historicalmap/map_compress.tif']),('gpkg', ['datasets/historicalmap/train.gpkg','datasets/historicalmap/train_centroid.gpkg'])]
 
  )
