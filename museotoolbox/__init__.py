ba21c38d582ba58773ca3f6536174586db476b88
Nicolas Karasiak <nicart@gmail.com>
2020-06-24 13:23:39 +0200

    init v0.13.6 for develop branch

--- museotoolbox/__init__.py
+++ museotoolbox/__init__.py
@@ -14,4 +14,4 @@
 # =============================================================================
 from . import ai, processing, cross_validation, datasets, stats
 
-__version__ = "0.13.5"
+__version__ = "0.13.6"
